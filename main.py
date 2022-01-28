8be53279602ce4c591b90037bea6e036dbca2914
rohitnawani <rohitnawan@gmail.com>
2022-01-28 13:55:07 -0500

    lint(*): fix pylint

--- main.py
+++ main.py
@@ -4,43 +4,46 @@ import re
 TABOO_URL = "https://playtaboo.com/ajax/v1/next/?"
 SAVED_CARDS = "taboo.txt"
 
-def parseOutput(url):
+def parse_output(url):
     '''
     Examples Input/Output
     Input: https://playtaboo.com/ajax/v1/next/?1624643694954
     Output: (GOVERNOR, ['STATE', 'REPRESENTATIVE', 'LEADER', 'EACH', 'ELECT'])
-    
-    Note: gameWordQuery is based on the string format of the current ajax query.
+
+    Note: game_word_query is based on the string format of the current ajax query.
     '''
-    string = str(urlopen(url).read())
+    string = ''
+    with urlopen(url) as response:
+        string = str(response.read())
 
-    gameWordQuery = r'game-word\\\\">' + '(.*?)' + r'<\\\\/h2>' # searches for word (any chars) between 2 raw strings
-    gameWord = re.search(gameWordQuery, string).group(1) # matches first ()-group finding
+    # searches for word (any chars) between 2 raw strings
+    game_word_query = r'game-word\\\\">' + '(.*?)' + r'<\\\\/h2>'
+    game_word = re.search(game_word_query, string).group(1) # matches first ()-group finding
 
-    listItemsQuery = r'<li>'+'(.*?)'+r'<\\\\/li>'
-    listWords = re.findall(listItemsQuery, string)
+    list_items_query = r'<li>'+'(.*?)'+r'<\\\\/li>'
+    list_words = re.findall(list_items_query, string)
 
-    return (gameWord, listWords)
+    return (game_word, list_words)
 
-def promptUser():
+def prompt_user():
     '''
     Prompt user for number of taboo cards to print.
     '''
     try:
-        numCards = int(input("How many taboo cards do you need? Enter number: "))
+        num_cards = int(input("How many taboo cards do you need? Enter number: "))
     except:
         print("That wasn't a number, try again?")
-        numCards = promptUser()
-    return numCards
+        num_cards = prompt_user()
+    return num_cards
 
 
-numCards = promptUser()
-print("Printing",numCards,"cards:")
+num_cards = prompt_user()
+print("Printing",num_cards,"cards:")
 
 # Print each taboo card and save output to file.
 with open(SAVED_CARDS, "a+") as file:
-    for num in range(numCards):
-        url = TABOO_URL + str(num)
-        data = parseOutput(url)
+    for num in range(num_cards):
+        URL = TABOO_URL + str(num)
+        data = parse_output(URL)
         file.write(str(data) + "\n")
         print(data)
